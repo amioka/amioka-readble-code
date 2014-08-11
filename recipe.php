e8f8ae06066cb4dd6fcb6e0072363ba127069881
weic-dev <weicdev@SAKAI-MBA13.local>
2014-08-11 11:58:40 +0900

    spec3:レシピデータを外部CSVに分離

--- recipe.php
+++ recipe.php
@@ -1,4 +1,18 @@
 <?php
 
-echo 'オムライス';
+const RECIPE_FILE = 'recipe_data.csv';
+
+$recipe_data = array();
+
+// レシピデータCSVを読み込む
+$fp = fopen(RECIPE_FILE, 'r');
+while ($line = fgetcsv($fp)) {
+	$recipe_data[] = $line;
+}
+fclose($fp);
+// 一行目はコメントなので捨てる
+array_shift($recipe_data);
+
+// レシピのタイトルを表示する
+echo $recipe_data[0][0];
 
